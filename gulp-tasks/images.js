2c06e35fe20fc1ac7f2f98b39b877176f52e9133
Alex K <oknetuzok@gmail.com>
2021-03-17 14:05:31 +0300

    upd modules
    - minor fix

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp')
-const changed = require('gulp-changed')
+const cached = require('gulp-cached')
 const imagemin = require('gulp-imagemin')
 const imageminPngquant = require('imagemin-pngquant')
 const browsersync = require('browser-sync')
 
 module.exports = function images() {
   return gulp.src('src/images/**/*.{gif,png,jpg,webp}')
-    .pipe(changed('build/img'))
+    .pipe(cached('imgCache'))
     .pipe(imagemin([
       imageminPngquant({ quality: [0.75, 0.75] }),
       imagemin.gifsicle({ interlaced: true }),
